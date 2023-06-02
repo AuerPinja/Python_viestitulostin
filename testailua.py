c0e136822d503657e0cc1d40a121e4e77a65b800
Pinja Auer <pinskumelissa@gmail.com>
2023-06-02 15:02:34 +0300

    testing modules

--- testailua.py
+++ testailua.py
@@ -1,3 +1,35 @@
+'''
+from fpdf import FPDF
 # moi
 
 print("Hello World!")
+
+pdf = FPDF()  
+
+pdf.add_page()
+  
+pdf.set_font('Courier', size=30)
+
+viesti = input("Kirjoita viesti: ")
+pdf.multi_cell(ln = 1, txt=viesti, align = 'c')
+
+tiedosto = input("Anna viestillesi tiedostonimi: ")  
+pdf.output(tiedosto+".pdf") 
+'''
+
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from textwrap import wrap
+
+message = input("Enter your message: ")
+tiedosto = input("Anna tiedostonimi: ")
+
+wraped_text = "\n".join(wrap(message, 80))
+
+pdf_file = tiedosto+".pdf"
+c = canvas.Canvas(pdf_file, pagesize=letter)
+c.setFont("Helvetica", 12)
+c.drawString(100, 700, message)
+c.save()
+
+print("Viesti on tallennettu!")
