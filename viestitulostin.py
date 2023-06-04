ac4c63f3eae75c6f57ab838985887ae06cdb143e
Pinja Auer <pinskumelissa@gmail.com>
2023-06-04 14:20:05 +0300

    added lahettaja

--- viestitulostin.py
+++ viestitulostin.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF
 
+
 pdf = FPDF()  
 
 pdf.add_page()
@@ -7,7 +8,11 @@ pdf.add_page()
 pdf.set_font('Courier', size=30)
 
 viesti = input("Kirjoita viesti: ")
+lahettaja = input("Kirjoita lähettäjän nimi")
+
 pdf.cell(ln = 1, txt=viesti, align = 'c')
+pdf.cell(txt=lahettaja)
 
 tiedosto = input("Anna viestillesi tiedostonimi: ")  
 pdf.output(tiedosto+".pdf")
+print("Viesti on tulostettu, ja se löytyy samasta kansiosta kuin tämä ohjelma.")
