1bff418bf8b16870cfe7b11bd29f8ca5292fe69e
Pinja Auer <pinskumelissa@gmail.com>
2023-06-04 14:51:43 +0300

    tried to do something with user input for colors

--- viestitulostin.py
+++ viestitulostin.py
@@ -8,8 +8,24 @@ pdf.add_page()
 viesti = input("Kirjoita viesti: ")
 lahettaja = input("Kirjoita lähettäjän nimi: ")
 
-color = 255,35,97
-  
+'''
+color = input("Anna tekstin väri (sininen, punainen tai vihreä): ")
+
+
+if color == "sininen":
+    color = 0,0,255
+elif color == "punainen":
+    color = 255,0,0
+elif color == "vihreä":
+    color = 0,255,0
+else: 
+    print("Väriä ei ole saatavilla! Saatavilla olevat värit ovat sininen, punainen tai vihreä.")
+'''
+
+
+
+color = 255, 0, 0
+
 pdf.set_font('Courier', size=30)
 pdf.set_text_color(color)
 
