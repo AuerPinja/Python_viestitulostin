62b33bd8ddd91bc26746bdf75fdc0ff673e25e94
Pinja Auer <pinskumelissa@gmail.com>
2023-06-04 14:09:44 +0300

    testailua

--- viestitulostin.py
+++ viestitulostin.py
@@ -7,7 +7,7 @@ pdf.add_page()
 pdf.set_font('Courier', size=30)
 
 viesti = input("Kirjoita viesti: ")
-pdf.multi_cell(ln = 1, txt=viesti, align = 'c')
+pdf.cell(ln = 1, txt=viesti, align = 'c')
 
 tiedosto = input("Anna viestillesi tiedostonimi: ")  
 pdf.output(tiedosto+".pdf")
