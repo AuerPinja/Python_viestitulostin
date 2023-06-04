0599e5c5887e3a23b1660f524f0dd9a7c7904f04
Pinja Auer <pinskumelissa@gmail.com>
2023-06-04 14:34:59 +0300

    added text color

--- viestitulostin.py
+++ viestitulostin.py
@@ -4,13 +4,17 @@ from fpdf import FPDF
 pdf = FPDF()  
 
 pdf.add_page()
-  
-pdf.set_font('Courier', size=30)
 
 viesti = input("Kirjoita viesti: ")
 lahettaja = input("Kirjoita lähettäjän nimi: ")
 
-pdf.cell(txt=viesti, align = 'c')
+color = 255,35,97
+  
+pdf.set_font('Courier', size=30)
+pdf.set_text_color(color)
+
+pdf.cell(txt=viesti)
+pdf.cell(txt=" ")
 pdf.cell(txt="Terveisin: " + lahettaja)
 
 tiedosto = input("Anna viestillesi tiedostonimi: ")  
